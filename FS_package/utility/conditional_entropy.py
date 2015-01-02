2ec36b256325f00645981aa93a2585a9cb2a76e8
jundongl <jundongl@asu.edu>
2015-01-01 22:08:57 -0700

    fix code format and comment in conditional_entropy

--- FS_package/utility/conditional_entropy.py
+++ FS_package/utility/conditional_entropy.py
@@ -22,5 +22,6 @@ def conditional_entropy(f1, f2):
     entropy_estimators.entropyd(x) is used to estimate the discrete entropy given a list of samples of discrete variable x
     entropy_estimators.midd(x,y) is used to estimate the mutual information between discrete variable x and y
     '''
+    
     ce = ee.entropyd(f1) - ee.midd(f1, f2)
     return ce
