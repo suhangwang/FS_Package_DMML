692c3ba108080db34a5fe6eea469d550e452ae0e
jundongl <jundongl@asu.edu>
2015-01-06 15:46:04 -0700

    fix data split method in supervised_evaluation

--- FS_package/utility/supervised_evaluation.py
+++ FS_package/utility/supervised_evaluation.py
@@ -1,66 +1,42 @@
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 from sklearn import cross_validation
 
 
-def evaluation_split(selected_features, y):
+def select_train_split(n_samples, train_size, n_iters):
     """
-    Calculate ACC of the selected features using 50-50 training-test split
+    This function implements the function splitting data into training set and test set evenly
     Input
     ----------
-        selectedFeatures: {numpy array}, shape (n_samples, n_selectedFeatures}
-            data of the selectedFeatures
-        Y: {numpy array}, shape (n_samples, 1)
-            true labels, guaranteed to be a numpy array
+    n_samples: {int}
+        number of samples in the dataset X
+    n_iter : {int}
+        parameter for the function cross_validation.ShuffleSplit
+    test_size : {int}
+        percentage of training data, ranging from 0 to 1
     Output
     ----------
-        classification accuracy: {float}
+    result after splitting the input data X
+        Output.train contains the index of samples for training
+        Output.test contains the index of samples for testing
     """
-
-    n_samples, n_features = selected_features.shape
-
-    # repeat 20 times, 50% for training and the rest 50% for testing (default)
-    ss = cross_validation.ShuffleSplit(n_samples, n_iter=20, test_size=0.5)
-
-    # 1-nearest neighbor (default)
-    neigh = KNeighborsClassifier(n_neighbors=1)
-
-    correct = 0
-    for train, test in ss:
-        neigh.fit(selected_features[train], y[train])
-        y_predict = neigh.predict(selected_features[test])
-        acc = accuracy_score(y[test], y_predict)
-        correct = correct + acc
-
-    return float(correct)/20
+    ss = cross_validation.ShuffleSplit(n_samples, n_iter=n_iters, test_size=train_size)
+    return ss
 
 
-def evaluation_leave_one(selected_features, y):
+def select_train_leave_one_out(n_samples):
     """
-    Calculate ACC of the selected features using leave-one-out cross validation
+    This function implements the function to select one sample for testing
     Input
     ----------
-        selectedFeatures: {numpy array}, shape (n_samples, n_selectedFeatures}
-            data of the selectedFeatures
-        Y: {numpy array}, shape (n_samples, 1)
-            actual labels
+    n_samples: {int}
+        number of samples in the dataset X
     Output
     ----------
-        classification accuracy: {float}
+    result after splitting the input data
+        Output.train contains the index of features for training
+        Output.test contains the index of features for testing
     """
+    loo = cross_validation.LeaveOneOut(n_samples)
+    return loo
 
-    n_samples, n_features = selected_features.shape
-    lo = cross_validation.LeaveOneOut(n_samples)
 
-    # 1-nearest neighbor (default)
-    neigh = KNeighborsClassifier(n_neighbors=1)
-
-    correct = 0
-    for train, test in lo:
-        neigh.fit(selected_features[train], y[train])
-        y_predict = neigh.predict(selected_features[test])
-        acc = accuracy_score(y[test], y_predict)
-        correct = correct + acc
-
-    return float(correct)/n_samples
 
