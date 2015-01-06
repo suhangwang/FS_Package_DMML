fee09abb5412f0186858a8a378daf2837504dff9
jundongl <jundongl@asu.edu>
2015-01-06 16:54:13 -0700

    add test_gini_index

---
+++ FS_package/test/test_gini_index.py
@@ -0,0 +1,45 @@
+import scipy.io
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from FS_package.function import gini_index
+from FS_package.utility import supervised_evaluation
+
+
+def main():
+    # load data
+    mat = scipy.io.loadmat('../data/COIL20.mat')
+    y = mat['gnd']
+    y = y[:, 0]
+    X = mat['fea']
+    n_samples, n_features = X.shape
+    X = X.astype(float)
+
+    # feature selection
+    W = gini_index.feature_select(X, y)
+    idx = gini_index.feature_ranking(W)
+    print idx
+
+    # split data
+    n_iter = 20
+    test_size = 0.5
+    ss = supervised_evaluation.select_train_split(n_samples, test_size, n_iter)
+
+    # cross validation
+    num_fea = 100
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    correct = 0
+
+    for train, test in ss:
+
+        score = gini_index.feature_select(X[train], y[train])
+        idx = gini_index.feature_ranking(score)
+        selected_features = X[:, idx[0:num_fea]]
+        neigh.fit(selected_features[train], y[train])
+        y_predict = neigh.predict(selected_features[test])
+        acc = accuracy_score(y[test], y_predict)
+        correct = correct + acc
+    print 'ACC', float(correct)/n_iter
+
+if __name__ == '__main__':
+    main()
+
