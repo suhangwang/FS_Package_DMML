e51d72258377f7b0a662cf05728c0bdeed9d0b9f
jundongl <jundongl@asu.edu>
2015-01-06 17:01:51 -0700

    fix NDFS

--- FS_package/function/NDFS.py
+++ FS_package/function/NDFS.py
@@ -1,9 +1,7 @@
-import scipy.io
 import sklearn.cluster
 import numpy as np
 import sys
-from utility.construct_W import construct_W
-from utility.unsupervised_evaluation import evaluation
+from ..utility.construct_W import construct_W
 
 
 def kmeans_initialization(X, n_clusters):
@@ -46,7 +44,7 @@ def calculate_obj(X, W, F, L, alpha, beta):
     return obj
     
 
-def NDFS(X, **kwargs):
+def feature_select(X, **kwargs):
     """
     This function implement the NDFS function
     
@@ -158,32 +156,3 @@ def feature_ranking(W):
     T = (W*W).sum(1)
     ind = np.argsort(T, 0)
     return ind[::-1]
-
-
-def main():
-    # load data
-    mat = scipy.io.loadmat('../data/COIL20.mat')
-    label = mat['gnd']    # label
-    label = label[:, 0]
-    X = mat['fea']    # data
-    X = X.astype(float)
-
-    kwargs = {"metric": "euclidean", "neighbor_mode": "knn", "weight_mode": "heat_kernel", "k": 5, 't': 1}
-    W = construct_W(X, **kwargs)
-    L = np.diag(W.sum(1)) - W
-
-    # feature weight learning / feature selection
-    W, obj = NDFS(X, n_clusters=40, L=L, verbose=1, max_iter=30)
-    idx = feature_ranking(W)
-    
-    # evaluation
-    num_features = 100
-    selected_features = X[:, idx[0:num_features]]
-    
-    ari, nmi, acc = evaluation(selected_features=selected_features, n_clusters=20, y=label)
-    print ari
-    print nmi
-    print acc
-    
-if __name__ == '__main__':
-    main()
