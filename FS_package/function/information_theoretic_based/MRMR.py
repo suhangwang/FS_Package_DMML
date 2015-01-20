19f4e4bfcabeaba6b8c7b4fc58759a2c5024e8f9
jundongl <jundongl@asu.edu>
2015-01-20 14:33:12 -0700

    add MRMR

---
+++ FS_package/function/information_theoretic_based/MRMR.py
@@ -0,0 +1,27 @@
+import LCSI
+
+
+
+def mrmr(X, y, **kwargs):
+    """
+    This function implements the mrmr function
+    Input
+    ----------
+    X: {numpy array}, shape (n_samples, n_features)
+        Input data, guaranteed to be a discrete numpy array
+    y : {numpy array},shape (n_samples, )
+        guaranteed to be a numpy array
+    kwargs: {dictionary}
+        n_selected_features:{int}
+            indicates the number of features to select
+    Output
+    ----------
+    F: {numpy array},shape (n_features, )
+        Index of selected features, F(1) is the most important feature.
+    """
+    if 'n_selected_features' in kwargs.keys():
+        n_selected_features = kwargs['n_selected_features']
+        F = LCSI.lcsi(X, y, gamma=0, function_name='MRMR', n_selected_features=n_selected_features)
+    else:
+        F = LCSI.lcsi(X, y, gamma=0, function_name='MRMR')
+    return F
