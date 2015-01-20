1143e05b6f897fda034a586c6c71d73e32edcad3
jundongl <jundongl@asu.edu>
2015-01-20 13:11:20 -0700

    update trace_ratio

--- FS_package/function/similarity_based/trace_ratio.py
+++ FS_package/function/similarity_based/trace_ratio.py
@@ -1,9 +1,8 @@
 import numpy as np
-from scipy.sparse import *
 from ...utility.construct_W import construct_W
 
 
-def trace_ratio(X, y, **kwargs):
+def trace_ratio(X, y, n_selected_features, **kwargs):
     """
     This function implements the trace ratio criterion for feature selection
 
@@ -13,6 +12,8 @@ def trace_ratio(X, y, **kwargs):
             Input data, guaranteed to be a numpy array
         y: {numpy array}, shape (n_samples, )
             True labels
+        n_selected_features: {int}
+            Number of features to select
         kwargs: {dictionary}
             style: {string}
                 style == 'fisher', build between-class matrix and within-class affinity matrix in a fisher score way
@@ -59,6 +60,39 @@ def trace_ratio(X, y, **kwargs):
     L_between = (np.transpose(L_between) + L_between)/2
     S_within = np.array(np.dot(np.dot(np.transpose(X), L_within), X))
     S_between = np.array(np.dot(np.dot(np.transpose(X), L_between), X))
+
+    # reflects the within-class or local affinity relationship encoded on Graph, Sw = X*Lw*X'
     S_within = (np.transpose(S_within) + S_within)/2
+    # reflects the between-class or global affinity relationship encoded on Graph, Sb = X*Lb*X'
     S_between = (np.transpose(S_between) + S_between)/2
+
+    # take the absolute values of diagonal
+    s_within = np.absolute(S_within.diagonal())
+    s_between = np.absolute(S_between.diagonal())
+    s_between[s_between == 0] = 1e-14  # this magic number if from authors' code
+
+    # preprocessing
+    fs_idx = np.argsort(np.divide(s_between, s_within), 0)[::-1]
+    k = np.sum(s_between[0:n_selected_features])/np.sum(s_within[0:n_selected_features])
+    s_within = s_within[fs_idx[0:n_selected_features]]
+    s_between = s_between[fs_idx[0:n_selected_features]]
+
+    # iterate util converge
+    while True:
+        score = np.sort(s_between-k*s_within)[::-1]
+        I = np.argsort(s_between-k*s_within)[::-1]
+        idx = I[0:n_selected_features]
+        old_k = k
+        k = np.sum(s_between[idx])/np.sum(s_within[idx])
+        if abs(k - old_k) < 1e-11:
+            break
+
+    # get feature index, feature-level score and subset-level score
+    feature_idx = fs_idx[I]
+    feature_score = score
+    subset_score = k
+
+    return feature_idx, feature_score, subset_score
+
+
 
