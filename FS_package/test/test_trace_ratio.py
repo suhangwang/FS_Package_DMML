834aaba4be55e7aea3faa2be673e837a32a7ebc2
jundongl <jundongl@asu.edu>
2015-01-20 13:11:42 -0700

    update test_trace_ratio

--- FS_package/test/test_trace_ratio.py
+++ FS_package/test/test_trace_ratio.py
@@ -1,4 +1,7 @@
 import scipy.io
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from FS_package.utility import supervised_evaluation
 from FS_package.function.similarity_based import trace_ratio
 
 
@@ -11,7 +14,24 @@ def main():
     n_samples, n_features = X.shape
     X = X.astype(float)
 
-    trace_ratio.trace_ratio(X, y, style='laplacian')
+    # split data
+    n_iter = 20
+    test_size = 0.5
+    ss = supervised_evaluation.select_train_split(n_samples, test_size, n_iter)
+
+    # cross validation
+    num_fea = 100
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    correct = 0
+
+    for train, test in ss:
+        idx, feature_score, subset_score = trace_ratio.trace_ratio(X, y, num_fea, style='fisher')
+        selected_features = X[:, idx]
+        neigh.fit(selected_features[train], y[train])
+        y_predict = neigh.predict(selected_features[test])
+        acc = accuracy_score(y[test], y_predict)
+        correct = correct + acc
+    print 'ACC', float(correct)/n_iter
 
 
 if __name__ == '__main__':
