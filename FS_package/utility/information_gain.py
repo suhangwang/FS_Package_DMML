b389436a2068c59e82bcbcdf104081bf7ce5cebb
jundongl <jundongl@asu.edu>
2015-01-20 16:03:44 -0700

    fix utility.information_gain

--- FS_package/utility/information_gain.py
+++ FS_package/utility/information_gain.py
@@ -5,13 +5,13 @@ import conditional_entropy as ce
 
 def information_gain(f1, f2):
     """
-    This function implement the function which calculates the information gain
+    This function implements the function which calculates the information gain
     Input
     ----------
-        f1: {numpy array}, shape (n_samples)
-            guaranteed to be a discrete data matrix
-        f2: {numpy array}, shape (n_samples)
-            guaranteed to be a discrete data matrix
+        f1: {numpy array}, shape (n_samples, )
+            guaranteed to be a discrete data array
+        f2: {numpy array}, shape (n_samples, )
+            guaranteed to be a discrete data array
     Output
     ----------
         ig: {float}
@@ -20,7 +20,6 @@ def information_gain(f1, f2):
     '''
     entropy_estimators.entropyd(x) is used to estimate the discrete entropy given a list of samples of discrete variable x
     '''
-
     # IG(f1,f2) = H(f1)-H(f1|f2)
     ig = ee.entropyd(f1) - ce.conditional_entropy(f1, f2)
     return ig
