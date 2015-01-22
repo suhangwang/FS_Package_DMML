0ef686fbaaefe8b88f870834c2e457438fa7aaba
jundongl <jundongl@asu.edu>
2015-01-22 12:19:36 -0700

    fix test_ls_l21_proximal

--- FS_package/test/test_ls_l21_proximal.py
+++ FS_package/test/test_ls_l21_proximal.py
@@ -1,4 +1,7 @@
 import scipy.io
+from FS_package.utility import supervised_evaluation
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 from FS_package.utility.sparse_learning import *
 from FS_package.function.sparse_learning_based import ls_l21_proximal
 
@@ -8,10 +11,30 @@ def main():
     mat = scipy.io.loadmat('../data/COIL20.mat')
     X = mat['fea']    # data
     X = X.astype(float)
-    label = mat['gnd']    # label
-    label = label[:, 0]
-    Y = construct_label_matrix(label)
-    ls_l21_proximal.proximal_gradient_descent(X, Y, 0.1)
+    y = mat['gnd']    # label
+    y = y[:, 0]
+    n_samples, n_features = X.shape
+
+    # split data
+    n_iter = 20
+    test_size = 0.5
+    ss = supervised_evaluation.select_train_split(n_samples, test_size, n_iter)
+
+    # cross validation
+    num_fea = 100
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    correct = 0
+
+    for train, test in ss:
+        Y = construct_label_matrix(y[train])
+        W, obj, value_gamma = ls_l21_proximal.proximal_gradient_descent_fast(X[train], Y, 0.1)
+        idx = ls_l21_proximal.feature_ranking(W)
+        selected_features = X[:, idx[0:num_fea]]
+        neigh.fit(selected_features[train], y[train])
+        y_predict = neigh.predict(selected_features[test])
+        acc = accuracy_score(y[test], y_predict)
+        correct = correct + acc
+    print 'ACC', float(correct)/n_iter
 
 
 if __name__ == '__main__':
