f7a1d75c252771b5dd89f8db0956b6406802c1d5
jundongl <jundongl@asu.edu>
2015-01-22 17:27:59 -0700

    fix test_ls_l21_proximal

--- FS_package/test/test_ls_l21_proximal.py
+++ FS_package/test/test_ls_l21_proximal.py
@@ -8,7 +8,7 @@ from FS_package.function.sparse_learning_based import ls_l21_proximal
 
 def main():
     # load data
-    mat = scipy.io.loadmat('../data/COIL20.mat')
+    mat = scipy.io.loadmat('../data/ORL.mat')
     X = mat['fea']    # data
     X = X.astype(float)
     y = mat['gnd']    # label
@@ -16,19 +16,19 @@ def main():
     n_samples, n_features = X.shape
 
     # split data
-    n_iter = 20
+    n_iter = 1
     test_size = 0.5
     ss = supervised_evaluation.select_train_split(n_samples, test_size, n_iter)
 
-    # cross validation
-    num_fea = 100
+    # evaluation
+    num_fea = 50
     neigh = KNeighborsClassifier(n_neighbors=1)
     correct = 0
 
     for train, test in ss:
-        Y = construct_label_matrix(y[train])
+        Y = construct_label_matrix_pan(y[train])
         W, obj, value_gamma = ls_l21_proximal.proximal_gradient_descent_fast(X[train], Y, 0.1, verbose=False)
-        idx = ls_l21_proximal.feature_ranking(W)
+        idx = feature_ranking(W)
         selected_features = X[:, idx[0:num_fea]]
         neigh.fit(selected_features[train], y[train])
         y_predict = neigh.predict(selected_features[test])
