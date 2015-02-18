507dd3e53a36c079968f0aa087ef2ffb468f4aa0
jundongl <jundongl@asu.edu>
2015-02-18 12:30:31 -0700

    update streaming method alpha_investing

--- FS_package/function/streaming/alpha_investing.py
+++ FS_package/function/streaming/alpha_investing.py
@@ -15,30 +15,31 @@ def alpha_investing(X, y, w0, dw):
     X_sel = np.ones(n_samples)
     clf = linear_model.LinearRegression(fit_intercept=False)
     for i in range(n_features):
-        X_can = X[:, i]
+        X_can = X[:, i]  # generate next feature
         alpha = w/2/(i+1)
         Xi = X[:, i]
         Xi_new = Xi.reshape(n_samples, 1)
         clf.fit(Xi_new, X_sel)
         r_squared = clf.score(Xi_new, X_sel)
         rho = np.sqrt(1-r_squared)
+        # compute corrected t-statistic
         abs = np.abs(np.inner(np.transpose(X_can), res))/np.sqrt(np.inner(np.transpose(X_can), X_can))/rho/sigma
         if np.isnan(abs):
             continue
         pval = 2-2*norm.cdf(abs)
-        if pval < alpha:
+        if pval < alpha:  # compare p-value to threshold
             X_tmp = np.concatenate((X_sel.reshape(n_samples, -1), X_can.reshape(n_samples, 1)), axis=1)
             clf.fit(X_tmp, y)
             if np.logical_not(np.all(np.logical_not(np.isnan(clf.coef_)))):
                 continue
-            sel = np.append(sel, (i+1))
+            sel = np.append(sel, (i+1))  # add feature to model
             X_sel = X_tmp
             y_predict = clf.predict(X_tmp)
             res = y - y_predict
             sigma = np.std(res)*np.sqrt(n_samples - 1)/np.sqrt(n_samples - 1 - len(sel))
             w = w + dw - alpha
-        else:
-            w = w - alpha
+        else:  # otherwise, reject
+            w -= alpha  # reduce wealth
         if w <= 0:
             break
     return sel.astype(int)
