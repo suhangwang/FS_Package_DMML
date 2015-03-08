bf7cb87f78f669fbec2c6e281c315001cf3d4d8e
jundongl <jundongl@asu.edu>
2015-03-07 17:40:40 -0700

    update alpha_investing

--- FS_package/function/streaming/alpha_investing.py
+++ FS_package/function/streaming/alpha_investing.py
@@ -1,45 +1,32 @@
 import numpy as np
-from scipy.stats import norm
 from sklearn import linear_model
 
 
 def alpha_investing(X, y, w0, dw):
-    y = y - np.mean(y)
     n_samples, n_features = X.shape
-    X = np.transpose(np.transpose(X)-np.transpose(np.tile(np.mean(X, axis=0), [n_samples, 1])))
-    sel = np.repeat(np.nan, 0)
-    # initials
-    res = y - np.mean(y)
-    sigma = np.std(res)
     w = w0
-    X_sel = np.ones(n_samples)
-    clf = linear_model.LinearRegression(fit_intercept=False)
+    F = []  # selected features
     for i in range(n_features):
-        X_can = X[:, i]  # generate next feature
+        x_can = X[:, i]  # generate next feature
         alpha = w/2/(i+1)
-        Xi = X[:, i]
-        Xi_new = Xi.reshape(n_samples, 1)
-        clf.fit(Xi_new, X_sel)
-        r_squared = clf.score(Xi_new, X_sel)
-        rho = np.sqrt(1-r_squared)
-        # compute corrected t-statistic
-        abs = np.abs(np.inner(np.transpose(X_can), res))/np.sqrt(np.inner(np.transpose(X_can), X_can))/rho/sigma
-        if np.isnan(abs):
-            continue
-        pval = 2-2*norm.cdf(abs)
-        if pval < alpha:  # compare p-value to threshold
-            X_tmp = np.concatenate((X_sel.reshape(n_samples, -1), X_can.reshape(n_samples, 1)), axis=1)
-            clf.fit(X_tmp, y)
-            if np.logical_not(np.all(np.logical_not(np.isnan(clf.coef_)))):
-                continue
-            sel = np.append(sel, (i+1))  # add feature to model
-            X_sel = X_tmp
-            y_predict = clf.predict(X_tmp)
-            res = y - y_predict
-            sigma = np.std(res)*np.sqrt(n_samples - 1)/np.sqrt(n_samples - 1 - len(sel))
+        X_old = X[:, F]
+
+        # model built with only X_old
+        logreg_old = linear_model.LogisticRegression()
+        logreg_old.fit(X_old, y)
+        error_old = 1 - logreg_old.score(X_old, y)
+
+        # model built with X_old & {x_can}
+        X_new = np.concatenate((X_old, x_can.reshape(n_samples, 1)), axis=1)
+        logreg_new = linear_model.LogisticRegression()
+        logreg_new.fit(X_new, y)
+        error_new = 1 - logreg_new.score(X_new, y)
+
+        # calculate p-value
+        pval = np.exp(-(error_old - error_new)/(2*error_old/n_samples))
+        if pval < alpha:
+            F.append(i)
             w = w + dw - alpha
-        else:  # otherwise, reject
-            w -= alpha  # reduce wealth
-        if w <= 0:
-            break
-    return sel.astype(int)
+        else:
+            w = w - alpha
+    return np.array(F)
--- FS_package/test/test_alpha_investing.py
+++ FS_package/test/test_alpha_investing.py
@@ -4,9 +4,10 @@ from FS_package.function.streaming import alpha_investing
 
 def main():
     # load matlab data
-    mat = scipy.io.loadmat('../data/housingexp.mat')
-    X = mat['X']  # data
-    y = mat['y']  # label
+    mat = scipy.io.loadmat('../data/COIL20.mat')
+    X = mat['fea']    # data
+    y = mat['gnd']    # label
+    y = y[:, 0]
     X = X.astype(float)
     y = y.astype(float)
     sel = alpha_investing.alpha_investing(X, y, 0.05, 0.05)
