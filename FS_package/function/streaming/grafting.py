57d6036719ca9f89c018f6392945888d93f4c52c
jundongl <jundongl@asu.edu>
2015-03-09 13:56:45 -0700

    update grafting

--- FS_package/function/streaming/grafting.py
+++ FS_package/function/streaming/grafting.py
@@ -6,8 +6,8 @@ def grafting(X, y, gamma):
     w = np.empty(0)
     free = np.empty(0)
     n_samples, n_features = X.shape
-    for i in n_features:
-        Xt = X[:, 0:i]
+    for i in range(n_features):
+        Xt = X[:, 0:(i+1)]
         Xty = np.dot(Xt.T, y)
         XtXt = np.dot(Xt.T, Xt)
         yy = np.dot(y.T, y)
@@ -19,19 +19,17 @@ def grafting(X, y, gamma):
             free = np.append(free, 1)
         else:
             free = np.append(free, 0)
-        w_tmp = minimize(lasso_obj, w[free > 0], XtXt[free > 0, free > 0], Xty[free > 0], yy, gamma)
-        w = np.zeros((i+1),)
-        w[free == 1] = w_tmp
-        free = np.abs(w) >= 1e-4
-
-        # grafting test for existing features
-        gradient_new = np.dot(XtXt, w)-Xty
-        for j in n_features:
-            if free[j] > 0 and np.abs(gradient[i]) < gamma:
-                free[j] = 0
-                w[j] = 0
+        if np.sum(free) > 0:
+            w_tmp = minimize(lasso_obj, w[free > 0], args=(XtXt[free > 0, free > 0], Xty[free > 0], yy, gamma))
+            w = np.zeros((i+1),)
+            w[free == 1] = w_tmp.x
+            free = np.abs(w) > 0
+    return np.nonzero(free)[0]
 
 
 def lasso_obj(w, XtXt, Xty, yy, gamma):
+    """
+    This function calculate the objective function of ||Xw-y||_{F}^{2}+\sum_{i}|w_{i}|
+    """
     f = np.sum(np.dot(np.dot(w, XtXt), w) - 2*np.dot(w, Xty) + yy) + gamma*np.sum(np.abs(w))
     return f
--- FS_package/test/test_grafting.py
+++ FS_package/test/test_grafting.py
@@ -1,3 +1,17 @@
 import scipy.io
 from FS_package.function.streaming import grafting
 
+
+def main():
+    # load matlab data
+    mat = scipy.io.loadmat('../data/USPS.mat')
+    X = mat['fea']    # data
+    y = mat['gnd']    # label
+    y = y[:, 0]
+    X = X.astype(float)
+    y = y.astype(float)
+    idx = grafting.grafting(X, y, 0.1)
+    print idx
+
+if __name__ == '__main__':
+    main()
