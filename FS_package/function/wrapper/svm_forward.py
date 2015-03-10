fdccd97a413c16de7aa65f588bd23db3821ec1d5
jundongl <jundongl@asu.edu>
2015-03-10 11:05:27 -0700

    update wrapper svm_forward

--- FS_package/function/wrapper/svm_forward.py
+++ FS_package/function/wrapper/svm_forward.py
@@ -29,11 +29,12 @@ def svm_forward(X, y, n_selected_features):
     clf = SVC()
 
     F = []  # selected feature set, initialized to be empty
-    for i in range(n_selected_features):
+    count = 0
+    while count < n_selected_features:
         max_acc = 0
-        for j in range(n_features):
-            if j not in F:
-                F.append(j)
+        for i in range(n_features):
+            if i not in F:
+                F.append(i)
                 X_tmp = X[:, F]
                 acc = 0
                 for train, test in cv:
@@ -46,7 +47,8 @@ def svm_forward(X, y, n_selected_features):
                 # record the feature with the largest accuracy
                 if acc > max_acc:
                     max_acc = acc
-                    idx = j
+                    idx = i
         # add the feature with the largest accuracy
         F.append(idx)
+        count += 1
     return np.array(F)
