12f17bf6d98d07cb7afbe52a22da279f470f9fb7
jundongl <jundongl@asu.edu>
2015-03-24 13:29:00 -0700

    update IGFS

--- FS_package/function/information_theoretic_based/IGFS.py
+++ FS_package/function/information_theoretic_based/IGFS.py
@@ -1,41 +1,23 @@
 import numpy as np
-from ...utility.information_gain import *
-
-
-def feature_ranking(F):
-    """
-    This function implements the function which sorts features according to their information gain in descending order
-
-    Input
-    -----
-    F: {numpy array}, shape (n_features,)
-        F[i] is the information gain value for the i-th feature
-
-    Output
-    ------
-    idx: {numpy array}, shape (n_features,)
-        index of sorted feature according to their information gain value in descending order
-    """
-    idx = np.argsort(F)
-    return idx[::-1]
+from ...utility.mutual_information import *
 
 
 def igfs(X, y):
     """
-    This function implements the function which selects features based on information gain value
+    This function implements information gain feature selection
 
     Input
     -----
     X: {numpy array}, shape (n_samples, n_features)
-        input data, guaranteed to be a discrete data array
+        input data
 
     y : {numpy array}, shape (n_samples,)
-        input label, guaranteed to be a discrete one-dimensional numpy array
+        input class labels
 
     Output
     ------
     F: {numpy array}, shape (n_features,)
-        F[i] is the information gain value for the i-th feature
+        information gain value for each feature
     """
 
     n_samples, n_features = X.shape
@@ -44,5 +26,10 @@ def igfs(X, y):
         f = X[:, i]
         F[i] = information_gain(f, y)
     return F
+
+
+def feature_ranking(F):
+    idx = np.argsort(F)
+    return idx[::-1]
 
 
