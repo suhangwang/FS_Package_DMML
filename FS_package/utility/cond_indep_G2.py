04f0118fb830dcdc5e2d1385461091d732e5616e
jundongl <jundongl@asu.edu>
2015-03-25 10:02:32 -0700

    update cond_indep_G2

--- FS_package/utility/cond_indep_G2.py
+++ FS_package/utility/cond_indep_G2.py
@@ -3,75 +3,68 @@ import csv
 from scipy import special
 
 
-def chisquared_prob(x2, v):
+def chi_squared_prob(x2, v):
     """
-    This function computes the chi-squared probability function.
-    P = chisquared_prob(X2,v) returns P(X2|v), the probability of observing a chi-squared value <= X2 with v degrees of freedom.
-    Input
-    ----------
-        x2: {int}
-            threshold number for chi-squared value
-        v: {int}
-            degrees of freedom
-    Output
-    ----------
-        p: {int}
-            the probability f observing a chi-squared value <= X2 with v degrees of freedom.
+    This function computes the chi-squared probability.
+    chisquared_prob(X2,v) returns P(X2|v), the probability of observing a chi-squared value <= X2 with v degrees of freedom.
     """
     return special.gammainc(v/2, float(x2)/2)
 
 
-def cond_indep_G2(Data, x, y, s):
+def cond_indep_G2(X, a, b, s):
     """
-    This function test if x is independent with y given s using likelihood ratio test G2
+    This function test if a is independent with b given s using likelihood ratio test G2
+
     Input
-    ----------
-        x: {int}
-            the index of variable x in Data matrix
-        y: {int}
-            the index of variable y in Data matrix
-        s:{{numpy array},shape (n_features, )}
-            the indexes of variables in set S
-        Data: {numpy array}, shape (n_features, n_samples)
-            Input data, guaranteed to be a discrete numpy array
+    -----
+    X: {numpy array}, shape (n_samples, n_features)
+        input data, guaranteed to be a discrete data matrix
+    a: {int}
+        index of variable a in the input X
+    b: {int}
+        index of variable y in Data matrix
+    s: {numpy array}, shape (n_features,)
+        indexes of variables in set s
+
     Output
-    ----------
+    ------
         ci: {int}
-            test result(1 = conditional independency, 0 = no)
-        G2 = {float}
-            G2 value(-1 if not enough data to perform the test -->CI=0)
-    ----------
+            test result (1 = conditional independent, 0 = no)
+        G2: {float}
+            G2 value(-1 if not enough data to perform the test, i.e., CI = 0)
+
+    Reference
+    ---------
+    Leray, Philippe and Francois, Olivier. "BNT Structure Learning Package: Documentation and Experiments" 2004
     """
-    Data = np.array(Data)
-    Data = np.transpose(Data)
-    Data = Data.astype(int)
+
     alpha = 0.05
-    n_samples, n_features = Data.shape
-    col_max = Data.max(axis=0)
+    n_samples, n_features = X.shape
+    col_max = X.max(axis=0)
     s_max = np.array([col_max[i] for i in s])
 
     if len(s) == 0:
-        n_ij = np.zeros((col_max[x], col_max[y]))
-        t_ij = np.zeros((col_max[x], col_max[y]))
-        df = np.prod(np.array([col_max[i] for i in np.array([x, y])])-1) * np.prod(s_max)
+        n_ij = np.zeros((col_max[a], col_max[b]))
+        t_ij = np.zeros((col_max[a], col_max[b]))
+        df = np.prod(np.array([col_max[i] for i in np.array([a, b])])-1) * np.prod(s_max)
     else:
         tmp = np.zeros(len(s_max))
         tmp[0] = 1
         tmp[1:] = np.cumprod(s_max[0:len(s_max)-1])
         qs = 1 + np.dot(s_max-1, tmp)
-        n_ijk = np.zeros((col_max[x], col_max[y], qs))
-        t_ijk = np.zeros((col_max[x], col_max[y], qs))
-        df = np.prod(np.array([col_max[i] for i in np.array([x, y])])-1)*qs
+        n_ijk = np.zeros((col_max[a], col_max[a], qs))
+        t_ijk = np.zeros((col_max[b], col_max[b], qs))
+        df = np.prod(np.array([col_max[i] for i in np.array([a, b])])-1)*qs
 
-    if (n_samples < 10*df):
+    if n_samples < 10*df:
         # not enough data to perform the test
         G2 = -1
         ci = 0
-    elif (len(s) == 0):
-        for i in range(col_max[x]):
-            for j in range(col_max[y]):
-                col = Data[:, 0]
-                n_ij[i, j] = len(col[(Data[:, x] == i+1) & (Data[:, y] == j+1)])
+    elif len(s) == 0:
+        for i in range(col_max[a]):
+            for j in range(col_max[b]):
+                col = X[:, 0]
+                n_ij[i, j] = len(col[(X[:, a] == i+1) & (X[:, b] == j+1)])
         col_sum_nij = np.sum(n_ij, axis=0)
         if len(col_sum_nij[col_sum_nij == 0]) != 0:
             temp_nij = np.array([])
@@ -84,10 +77,10 @@ def cond_indep_G2(Data, x, y, s):
         col = np.sum(n_ij, axis=0)
         for i in range(len(row)):
             for j in range(len(col)):
-                t_ij[i,j] = float(row[i] * col[j])/n_samples
-        tmp = np.zeros((col_max[x], col_max[y]))
-        for i in range(col_max[x]):
-            for j in range(col_max[y]):
+                t_ij[i, j] = float(row[i] * col[j])/n_samples
+        tmp = np.zeros((col_max[a], col_max[b]))
+        for i in range(col_max[a]):
+            for j in range(col_max[b]):
                 if t_ij[i, j] == 0:
                     tmp[i, j] = float('Inf')
                 else:
@@ -96,27 +89,25 @@ def cond_indep_G2(Data, x, y, s):
         tmp = 2 * n_ij * np.log(tmp)
 
         G2 = np.sum(tmp)
-        alpha2 = 1 - chisquared_prob(G2, df)
+        alpha2 = 1 - chi_squared_prob(G2, df)
         ci = (alpha2 >= alpha)
-        return int(ci)
-
     else:
         for example in range(n_samples):
-            i = Data[example, x]
-            j = Data[example, y]
-            si = Data[example, [element for element in s]]-1
+            i = X[example, a]
+            j = X[example, b]
+            si = X[example, [element for element in s]]-1
             k = int(1+np.dot(si, tmp))
-            n_ijk[i-1, j-1, k-1] = n_ijk[i-1, j-1, k-1] + 1
+            n_ijk[i-1, j-1, k-1] += 1
         n_ik = np.sum(n_ijk, axis=1)
         n_jk = np.sum(n_ijk, axis=0)
         n2 = np.sum(n_jk, axis=0)
-        tmp = np.zeros((col_max[x], col_max[y], qs))
+        tmp = np.zeros((col_max[a], col_max[b], qs))
         for k in range(int(qs)):
             if n2[k] == 0:
                 t_ijk[:, :, k] = 0
             else:
-                for i in range(col_max[x]):
-                    for j in range(col_max[y]):
+                for i in range(col_max[a]):
+                    for j in range(col_max[b]):
                         if n2[k] == 0:
                             t_ijk[i, j, k] = float('Inf')
                         else:
@@ -128,9 +119,10 @@ def cond_indep_G2(Data, x, y, s):
         tmp[(tmp == float('Inf')) | (tmp == 0)] = 1
         tmp = 2 * n_ijk * np.log(tmp)
         G2 = np.sum(tmp)
-        alpha2 = 1 - chisquared_prob(G2, df)
+        alpha2 = 1 - chi_squared_prob(G2, df)
         ci = (alpha2 >= alpha)
-        return int(ci)
+
+    return int(ci), G2
 
 
 def main():
